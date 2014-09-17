b6bcbbcac3f65f3dcc0993b30f60fcf36ac8ed8e
Sascha <sascha@schmidt.is>
2014-09-17 17:18:57 +0200

    Adapted to latest changes to the paper and optimized memory usage

--- src/catena-DBG.c
+++ src/catena-DBG.c
@@ -26,6 +26,25 @@ void XOR(const uint8_t *input1, const uint8_t *input2, uint8_t *output)
   }
 }
 
+/*calculate actual index from level and element index*/
+uint64_t ind(uint16_t i, uint64_t j, uint64_t c, uint32_t m){
+  if(i % 3 == 0){
+    return j;
+  }
+  else if(i % 3 == 1){
+    if(j < m){
+      return j + c;
+    }
+    else{
+      return j - m;
+    }
+  }
+  else{ //i % 3 == 2
+    return j + m;
+  }
+
+}
+
 /* Computes the hash of x using a Double Butterfly Graph,
 * that forms as (2^g,\lamba)-Superconcentrator
 */
@@ -33,37 +52,40 @@ void F(const uint8_t x[H_LEN], const uint8_t lambda,
     const uint8_t garlic,   uint8_t h[H_LEN])
 {
   const uint64_t c = UINT64_C(1) << garlic;
-  const uint64_t l = 2 * garlic;
+  const uint32_t m = UINT32_C(1) << (garlic-1);
+  const uint16_t l = 2 * garlic;
 
-  uint8_t *v = malloc(c*H_LEN);
-  uint8_t *r = malloc(c*H_LEN);
+  uint8_t *r = malloc((c+m)*H_LEN);
+  uint8_t *tmp = malloc(H_LEN);
 
-  uint64_t i, j;
-  uint32_t k;
+  uint16_t i;
+  uint64_t j;
+  uint8_t k;
 
-  //top row: v is the input level of the first iteration
-  __Hash1(x, H_LEN, v);
+  //top row
+  __Hash1(x, H_LEN, r);
   for(j = 1; j < c; j++){
-    __Hash1(v + (j-1)*H_LEN, H_LEN, v + j*H_LEN);
+    __Hash1(r + (j-1)*H_LEN, H_LEN, r + j*H_LEN);
   }
 
   //iterations
   for (k = 0; k < lambda; k++) {
     //levels
     for(i=1; i < l; i++){
-      XOR(v, v + sigma(garlic,i-1,0) * H_LEN, r);
-      __Hash2(v + (c-1)*H_LEN, H_LEN, r, H_LEN, r);
-
+      XOR(r + ind(i-1, 0, c,m) * H_LEN, 
+        r + ind(i-1,c-1,c,m)*H_LEN, tmp);
+      __Hash2(r + ind(i-1,sigma(garlic,i-1,0),c,m) * H_LEN, H_LEN, tmp,
+         H_LEN, r + ind(i,0,c,m) *H_LEN);
+      //elements
       for(j = 1; j < c; j++){
-        XOR(v + j * H_LEN, v + sigma(garlic,i-1,j) * H_LEN, r + j * H_LEN);
-        __Hash2(r + (j-1)*H_LEN, H_LEN, r + j * H_LEN, H_LEN, r + j * H_LEN);
+        XOR(r + ind(i-1,j,c,m) * H_LEN, r + ind(i,j-1,c,m)*H_LEN, tmp);
+        __Hash2(r + ind(i-1,sigma(garlic,i-1,j),c,m) * H_LEN, H_LEN, tmp, 
+          H_LEN, r + ind(i,j,c,m) * H_LEN);
       }
-
-      memcpy(v, r, c * H_LEN); //v <-r
     }
+    memcpy(r,r +ind(i-1,0,c,m) * H_LEN, c * H_LEN);
   }
 
-  memcpy(h, v + (c - 1) * H_LEN, H_LEN);
+  memcpy(h, r + ind(l-1,c-1,c,m) * H_LEN, H_LEN);
   free(r);
-  free(v);
 }
