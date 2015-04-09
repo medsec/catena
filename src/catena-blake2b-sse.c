4147e107b70e5eaf130b2c8cfaabcae5f9068f04
funkysash <sascha@schmidt.is>
2015-04-09 14:36:20 +0200

    Minor speed improvement by using the optimizations Blake2b uses on little endian systems

--- src/catena-blake2b-sse.c
+++ src/catena-blake2b-sse.c
@@ -1,4 +1,10 @@
 #include "catena.h"
+//Loading in blake2b is faster on little endian
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+#ifndef NATIVE_LITTLE_ENDIAN
+  #define NATIVE_LITTLE_ENDIAN
+#endif
+#endif
 #include "blake2-sse/blake2.h"
 #include "hash.h"
 
